f725865409402eec0be761a2d42aa44f161008ad
Soberanalysts <106496733+Soberanalysts@users.noreply.github.com>
2025-01-27 02:13:38 +0900

    [level 0] Title: 홀짝 구분하기, Time: 51.08 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -10,9 +10,4 @@ rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
     n = Number(input[0]);
-    if(n % 2 === 0) {
-        console.log(`${n} is even`);
-    } else {
-        console.log(`${n} is odd`);
-    }
 });
