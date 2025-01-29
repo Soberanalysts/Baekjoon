d486a4aeebb2522586543be58d6882d9af70d14f
Soberanalysts <106496733+Soberanalysts@users.noreply.github.com>
2025-01-29 18:48:42 +0900

    [level 0] Title: 문자열 돌리기, Time: 47.42 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    answer = '';
+    for (let char of str) {
+        answer = char;
+        console.log(answer);
+    }
+});
